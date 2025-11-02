bfea2fabe8c01d6026f2b6d9d356b87ebc550daa
Daniel Na <element11sodium@gmail.com>
2025-11-02 17:00:43 -0500

    L1.4

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void) {
     
@@ -9,14 +10,32 @@ int main(void) {
     }
     
     const size_t buf_size = 8;
-    unsigned char buf[buf_size+1];
+    char buf[buf_size+1];
     buf[buf_size] = '\0';
     size_t bytes_read;
     
+    const size_t max_line = 1024;
+    char line[max_line];
+    line[0] = '\0';
+    
     while((bytes_read = fread(buf, 1, buf_size, file)) != 0) {
-        if (bytes_read != 8)
+        if (bytes_read != 8) {
             buf[bytes_read] = '\0';
-        printf("read: %s\n", buf);
+	    strcat(line, buf);
+            printf("read: %s\n", line);
+	    break;
+        }
+
+	char* split = strtok(buf, "\n");
+	int breaks = 0;
+	while (split != NULL) {
+	    strcat(line, split);
+	    split = strtok(NULL, "\n");
+	    if (split != NULL) {
+		printf("read: %s\n", line);
+		line[0] = '\0';
+	    }
+	}
     }
 
     return 0;
